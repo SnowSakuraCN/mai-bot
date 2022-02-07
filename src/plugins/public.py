5ca806f918e0368887de369a9adf05850d387b29
SnowSakura <71383712+SnowSakuraCN@users.noreply.github.com>
2022-02-08 00:03:25 +0800

    Update public.py

--- src/plugins/public.py
+++ src/plugins/public.py
@@ -10,15 +10,7 @@ from nonebot.message import event_preprocessor
 from src.libraries.image import *
 from random import randint
 
-
-@event_preprocessor
-async def preprocessor(bot, event, state):
-    if hasattr(event, 'message_type') and event.message_type == "private" and event.sub_type != "friend":
-        raise IgnoredException("not reply group temp message")
-
-        
 help = on_command('help')
-
 
 @help.handle()
 async def _(bot: Bot, event: Event, state: T_State):
@@ -38,24 +30,3 @@ XXXmaimaiXXX什么 随机一首歌
             "file": f"base64://{str(image_to_base64(text_to_image(help_str)), encoding='utf-8')}"
         }
     }]))
-
-
-async def _group_poke(bot: Bot, event: Event, state: dict) -> bool:
-    value = (event.notice_type == "notify" and event.sub_type == "poke" and event.target_id == int(bot.self_id))
-    return value
-
-
-poke = on_notice(rule=_group_poke, priority=10, block=True)
-
-
-@poke.handle()
-async def _(bot: Bot, event: Event, state: T_State):
-    if event.__getattribute__('group_id') is None:
-        event.__delattr__('group_id')
-    await poke.send(Message([{
-        "type": "poke",
-        "data": {
-            "qq": f"{event.sender_id}"
-        }
-    }]))
-
