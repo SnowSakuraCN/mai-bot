29242de2db2913418e77a2d50fb03bb0e0d4cf40
SnowSakura <71383712+SnowSakuraCN@users.noreply.github.com>
2022-02-08 00:43:52 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,8 +12,8 @@ app = nonebot.get_asgi()
 driver = nonebot.get_driver()
 driver.register_adapter(Adapter)
 
-#nonebot.load_plugins("src/plugins")
-nonebot.load_all_plugins([],"src/plugins")
+nonebot.load_plugins([],"src/plugins")
+#nonebot.load_all_plugins([],"src/plugins")
 
 if __name__ == "__main__":
     nonebot.run(app="bot:app")
