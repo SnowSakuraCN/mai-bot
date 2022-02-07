73925f6a98a3a4f45469e390bda859e950f70333
SnowSakura <71383712+SnowSakuraCN@users.noreply.github.com>
2022-02-08 00:49:07 +0800

    Update public.py

--- src/plugins/public.py
+++ src/plugins/public.py
@@ -2,26 +2,17 @@ import random
 import re
 
 from PIL import Image
-from nonebot import on_command, on_message, on_notice, require, get_driver, on_regex
-from nonebot.typing import T_State
+from nonebot.matcher import Matcher
+from nonebot import on_command
 from nonebot.adapters.onebot.v11 import Message, Event, Bot
-from nonebot.exception import IgnoredException
-from nonebot.message import event_preprocessor
 from src.libraries.image import *
 from random import randint
 
-
-@event_preprocessor
-async def preprocessor(bot, event, state):
-    if hasattr(event, 'message_type') and event.message_type == "private" and event.sub_type != "friend":
-        raise IgnoredException("not reply group temp message")
-
-        
 help = on_command('help')
 
 
 @help.handle()
-async def _(bot: Bot, event: Event, state: T_State):
+async def _(matcher: Matcher):
     help_str = '''可用命令如下：
 今日舞萌 查看今天的舞萌运势
 XXXmaimaiXXX什么 随机一首歌
@@ -32,29 +23,9 @@ XXXmaimaiXXX什么 随机一首歌
 定数查歌 <定数>  查询定数对应的乐曲
 定数查歌 <定数下限> <定数上限>
 分数线 <难度+歌曲id> <分数线> 详情请输入“分数线 帮助”查看'''
-    await help.send(Message([{
+    await matcher.send(Message([{
         "type": "image",
         "data": {
             "file": f"base64://{str(image_to_base64(text_to_image(help_str)), encoding='utf-8')}"
-        }
-    }]))
-
-
-async def _group_poke(bot: Bot, event: Event, state: dict) -> bool:
-    value = (event.notice_type == "notify" and event.sub_type == "poke" and event.target_id == int(bot.self_id))
-    return value
-
-
-poke = on_notice(rule=_group_poke, priority=10, block=True)
-
-
-@poke.handle()
-async def _(bot: Bot, event: Event, state: T_State):
-    if event.__getattribute__('group_id') is None:
-        event.__delattr__('group_id')
-    await poke.send(Message([{
-        "type": "poke",
-        "data": {
-            "qq": f"{event.sender_id}"
         }
     }]))
