369278e483e0061559ce9ea196205f8d3e6d7087
SnowSakura <71383712+SnowSakuraCN@users.noreply.github.com>
2022-02-08 00:45:34 +0800

    Delete src/plugins/nonebot_plugin_maimai directory

--- src/plugins/nonebot_plugin_maimai/__init__.py
+++
@@ -1,8 +0,0 @@
-from nonebot import on_command
-from nonebot.matcher import Matcher
-
-help = on_command('help')
-
-@help.handle()
-async def send_help(matcher: Matcher):
-    await matcher.send(message="QWQ")
