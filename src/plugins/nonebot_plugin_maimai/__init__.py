10aa32af0e1decf2a674570b77074912375c140a
SnowSakura <71383712+SnowSakuraCN@users.noreply.github.com>
2022-02-08 00:37:53 +0800

    Update __init__.py

--- src/plugins/nonebot_plugin_maimai/__init__.py
+++ src/plugins/nonebot_plugin_maimai/__init__.py
@@ -1,1 +1,8 @@
+from nonebot import on_command
+from nonebot.matcher import Matcher
 
+help = on_command('help')
+
+@help.handle()
+async def send_help(matcher: Matcher):
+    await matcher.send(message="QWQ")
